33bc4c33b9aca12c3ce4c73b916746769e1855a2
meethatkar <meethatkar000@gmail.com>
2026-08-03 11:56:06 +0530

    feat: Added Context layer.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "../app.routes";
+import { AuthProvider } from "./features/auth/auth.context";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 }
 
 export default App;
